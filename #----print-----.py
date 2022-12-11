c11baacf37d0c185bfc45c1188e2587c765b2bcf
KRISHNARAJ007-R <86521717+KRISHNARAJ007-R@users.noreply.github.com>
2022-12-11 20:21:15 +0530

    Add files via upload

---
+++ #----print-----.py
@@ -0,0 +1,11 @@
+#----print-----
+print("Krishna")
+
+#-------variables----
+a="Krishna"
+print(a)
+print("HI! " + a)            #C is faster than Python because it's exceute entire where in python it is intrepted line by line
+b=25
+c=4.5
+print(b,c)                   #b and c value is print in one line using comma
+                             #dynamically typied lanugage
